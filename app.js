a5fa3f7caf3f47aac7c0cfc25a5e69877ef5c670
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-11-24 18:32:01 -0600

    initial commit

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ function teamMenu() {
           },
         },
       ])
-      .then(answers => {
+      .then((answers) => {
         const manager = new Manager(
           answers.managerName,
           answers.managerId,
@@ -81,33 +81,34 @@ function teamMenu() {
         employees.push(manager);
         idArray.push(answers.managerId);
         createTeam();
-      })
+      });
   }
   function createTeam() {
-    inquirer.prompt([
-      {
-        type: "list",
-        name: "memberChoice",
-        message: "What is the role of your team member?",
-        choice: [
-          "Engineer",
-          "Intern",
-          "I do not want to add another team member.",
-        ],
-      },
-    ]).then(choice => {
-      switch(choice.memberChoice){
-        case "Engineer": 
-        addEngineer();
-        break;
-        case "Intern": 
-        addIntern();
-        break;
-        default: 
-        buildTeam();
-
-      }
-    })
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "memberChoice",
+          message: "What is the role of your team member?",
+          choice: [
+            "Engineer",
+            "Intern",
+            "I do not want to add another team member.",
+          ],
+        },
+      ])
+      .then((choice) => {
+        switch (choice.memberChoice) {
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+          default:
+            buildTeam();
+        }
+      });
     //addENG
     function createEngineer() {
       console.log("Please input employee's info");
@@ -160,64 +161,136 @@ function teamMenu() {
             },
           },
         ])
-        .then(answers => {
+        .then((answers) => {
           const engineer = new Engineer(
             answers.engineerName,
             answers.engineerId,
             answers.engineerEmail,
-            answers.engineerGithub,
+            answers.engineerGithub
           );
           employees.push(engineer);
           idArray.push(answers.engineerId);
           createTeam();
-        })
+        });
     }
     function createTeam() {
-      inquirer.prompt([
-        {
-          type: "list",
-          name: "memberChoice",
-          message: "What is the role of your team member?",
-          choice: [
-            "Engineer",
-            "Intern",
-            "I do not want to add another team member.",
-          ],
-        },
-      ]).then(choice => {
-        switch(choice.memberChoice){
-          case "Engineer": 
-          addEngineer();
-          break;
-          case "Intern": 
-          addIntern();
-          break;
-          default: 
-          buildTeam();
-  
-        }
-      })
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "memberChoice",
+            message: "What is the role of your team member?",
+            choice: [
+              "Engineer",
+              "Intern",
+              "I do not want to add another team member.",
+            ],
+          },
+        ])
+        .then((choice) => {
+          switch (choice.memberChoice) {
+            case "Engineer":
+              addEngineer();
+              break;
+            case "Intern":
+              addIntern();
+              break;
+            default:
+              buildTeam();
+          }
+        });
+      function createIntern() {
+        console.log("Please input intern's info");
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "internName",
+              message: "Enter Intern's name.",
+              validate: (answer) => {
+                if (answer !== "") {
+                  return true;
+                }
+                return "Please enter employees name";
+              },
+            },
+            {
+              type: "input",
+              name: "internId",
+              message: "Enter employee id.",
+              validate: (answer) => {
+                const pass = answer.match(/^[0-9]\d*$/);
+                if (pass) {
+                  return true;
+                }
+                return "Please enter id.";
+              },
+            },
+            {
+              type: "input",
+              name: "internEmail",
+              message: "Enter employee's email address",
+              validate: (answer) => {
+                const pass = answer.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+                if (pass) {
+                  return true;
+                }
+                return "Please enter a valid email address.";
+              },
+            },
+            {
+              type: "input",
+              name: "internSchool",
+              message: "Please enter Intern's School name.",
+              validate: (answer) => {
+                if (answer !== "") {
+                  return true;
+                }
+                return "Please enter a school name";
+              },
+            },
+          ])
+          .then((answers) => {
+            const intern = new Intern(
+              answers.internName,
+              answers.internId,
+              answers.internEmail,
+              answers.internSchool
+            );
+            employees.push(intern);
+            idArray.push(answers.internId);
+            createTeam();
+          });
+      }
+      function createTeam() {
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "memberChoice",
+              message: "What is the role of your team member?",
+              choice: [
+                "Engineer",
+                "Intern",
+                "I do not want to add another team member.",
+              ],
+            },
+          ])
+          .then((choice) => {
+            switch (choice.memberChoice) {
+              case "Engineer":
+                addEngineer();
+                break;
+              case "Intern":
+                addIntern();
+                break;
+              default:
+                buildTeam();
+            }
+            createManager();
+          });
+      }
+    }
   }
-  createManager();
-}
-teamMenu();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+ }
+ teamMenu();
